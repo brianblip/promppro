f7570c519e20ce82d438625e32b14f75567450dc
Brian Prado <brravee123@gmail.com>
2023-10-12 09:04:09 +0800

    fixbugs

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -19,9 +19,9 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
 	const handleProfileClick = () => {
 		console.log(post);
-		if (post.creator._id === session?.user.id) return router.push("/Profile");
+		if (post.creator._id === session?.user.id) return router.push("/profile");
 
-		router.push(`/Profile/${post.creator._id}?name=${post.creator.username}`);
+		router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
 	};
 	return (
 		<div className="prompt_card">
@@ -65,7 +65,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 			>
 				#{post.tag}
 			</p>
-			{session?.user.id === post.creator._id && pathName === "/Profile" && (
+			{session?.user.id === post.creator._id && pathName === "/profile" && (
 				<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
 					<p
 						className="font-inter text-sm green_gradient cursor-pointer"
