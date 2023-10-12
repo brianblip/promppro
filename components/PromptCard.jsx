60c62f3414d4848a0de45bb85dc608d3f1c61ea9
Brian Prado <brravee123@gmail.com>
2023-10-12 08:59:29 +0800

    profileEdit

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -65,7 +65,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 			>
 				#{post.tag}
 			</p>
-			{session?.user.id === post.creator._id && pathName === "/profile" && (
+			{session?.user.id === post.creator._id && pathName === "/Profile" && (
 				<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
 					<p
 						className="font-inter text-sm green_gradient cursor-pointer"
